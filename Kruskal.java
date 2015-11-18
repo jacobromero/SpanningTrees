c905a7d38e2e751b7e1e5f44a0ca552999226fdc
jacobromero <jacob22romero@gmail.com>
2015-11-17 22:16:56 -0800

    added some basic methods to kruskal.java, the will be implemented later on.

--- Kruskal.java
+++ Kruskal.java
@@ -1,4 +1,18 @@
 
 public class Kruskal {
+	//???????????????????
+	public int[] edges;
 	
+	
+	public void krusAlgo(){
+		
+	}
+	
+	public void heapSort(){
+		
+	}
+	
+	public void merge3(int indexA, int indexB){
+		
+	}
 }
